86f40f12f5afda6c8315f7a28f6a20072951a01b
VishalMer <vishalmer99747@gmail.com>
2025-01-23 18:26:48 +0530

    Added more files

---
+++ Other/employee.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4BCAA.Others
+{
+    public class employee
+    {
+        int id, age;
+        double salary;
+        string name;
+
+        public void getData()
+        {
+            Console.Write("Enter employee id : ");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter employee name : ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter employee age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter employee salary : ");
+            salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("id of the employee is : " + id);
+            Console.WriteLine("name of the employee is : " + name);
+            Console.WriteLine("age of the employee is : " + age);
+            Console.WriteLine("salary of the employee is : " + salary);
+
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class employee2
+    {
+        public static void Main(string[] args)
+        {
+            employee e1 = new employee();
+            employee e2 = new employee();
+            Console.WriteLine("Enter employee 1 details : ");
+            e1.getData();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter employee 2 details : ");
+            e2.getData();
+            Console.WriteLine();
+
+            Console.WriteLine("Employee 1 details : ");
+            e1.showData();
+            Console.WriteLine();
+
+            Console.WriteLine("Employee 2 details : ");
+            e2.showData();
+            Console.WriteLine();
+
+
+            if (e1.getAge() > e2.getAge())
+            {
+                Console.WriteLine(e1.getName() + " is older.");
+            }
+            else
+            {
+                Console.WriteLine(e2.getName() + " is older.");
+            }
+        }
+    }
+}
---
+++ Other/product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4BCAA.Assignment_1
+{
+    public class product
+    {
+        int p_id;
+        string p_name;
+        double p_price;
+
+        public product()
+        {
+            Console.WriteLine("Enter productid: ");
+            p_id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter product name: ");
+            p_name = Console.ReadLine();
+
+            Console.WriteLine("Enter price: ");
+            p_price = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Product ID : " + p_id);
+            Console.WriteLine("Product Name : " + p_name);
+            Console.WriteLine("Product Price : " + p_price);
+        }
+    }
+
+    public class productTest
+    {
+        static void Main(string[] args)
+        {
+            product [] p = new product[5];
+            for (int i = 0; i<5; i++)
+            {
+                p[i] = new product();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                p[i].showData();
+            }
+        }
+    }
+}
