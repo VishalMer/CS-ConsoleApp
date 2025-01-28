76eb492cad295e7d12a24231279c9b5fdfbd37e1
VishalMer <vishalmer99747@gmail.com>
2025-01-28 11:42:55 +0530

    Updated...

---
+++ Other/Time.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_P1.Other
+{
+    public class Time
+    {
+        int hour, minute, second;
+        string time;
+
+        public Time(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+
+        void increment()
+        {
+            second++;
+            if (second >= 60)
+            {
+                second = 0;
+                minute++;
+                if (minute >= 60)
+                {
+                    minute = 0;
+                    hour++;
+                    if (hour >= 24)
+                    {
+                        hour = 0;
+                    }
+                }
+            }
+        }
+
+        public void DisplayTime()
+        {
+            time = (hour + ":" + minute + ":" + second);
+            Console.WriteLine("Time :" + time);
+        }
+
+        public static void Main(string[] args)
+        {
+            Time t = new Time(11, 50, 59);
+            t.DisplayTime();
+            t.increment();
+            t.DisplayTime();
+        }
+    }
+}
---
+++ Other/incTime.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_P1.Other
+{
+    internal class incTime
+    {
+        public class Time
+        {
+            int sec;
+            int min;
+            int hr;
+            string t = "";
+
+            public Time()
+            {
+
+                Console.Write("Enter the hour : ");
+                hr = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the minute : ");
+                min = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the second : ");
+                sec = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Current time is : " + hr + ":" + min + ":" + sec);
+
+                Console.WriteLine();
+
+
+            }
+
+            string increment()
+            {
+                sec = sec + 1;
+
+                if (sec >= 60)
+                {
+                    sec = 0;
+                    min = min + 1;
+
+                    if (min >= 60)
+                    {
+                        hr = hr + 1;
+                        min = 0;
+                    }
+                }
+
+                return t = hr + ":" + min + ":" + sec;
+
+                //Console.WriteLine("Time is : " + t);
+
+            }
+
+
+            static void Main(string[] args)
+            {
+
+                Time t1 = new Time();
+
+                string incTime = t1.increment();
+
+                Console.WriteLine("New time is : " + incTime);
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
+
+
